7716c34e08f021ab86a422a55638300465bbebf5
player0-glitch <mokhaditlhalefo@gmail.com>
2025-07-29 15:39:16 +0200

    feat(renderer): added mesh drawing in favour of shape drawing

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,11 +1,12 @@
 #include "../include/Renderer.h"
 #include "Engine.h"
 #include "SDL2/SDL_error.h"
+#include "SDL2/SDL_rect.h"
 #include "SDL2/SDL_render.h"
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 #include <SDL_error.h>
 #include <SDL_rect.h>
-#include <SDL_video.h>
 #include <iostream>
 #include <v3Util.h>
 // These variables are used by the main render window
@@ -25,6 +26,24 @@ Renderer::Renderer(const char *windowTitle, int windowWidth, int windowHeight) {
   _renderer = SDL_CreateRenderer(
       _window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 }
+void Renderer::draw(const Mesh &shape) {
+  // This is a very basic renderer that draws triangles
+  // It does not handle textures or lighting
+  // It only draws the triangles in the mesh
+  // TODO This is our point of interest
+
+  for (const Triangle &tri : shape.tris) {
+
+    SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255);
+
+    // Draw the triangle
+    SDL_RenderDrawLine(_renderer, tri.p1.x, tri.p1.y, tri.p2.x, tri.p2.y);
+    SDL_RenderDrawLine(_renderer, tri.p2.x, tri.p2.y, tri.p3.x, tri.p3.y);
+    // SDL_RenderDrawLine(_renderer, point3.x, point3.y, point1.x, point1.y);
+  }
+  SDL_RenderPresent(_renderer);
+}
+
 void Renderer::drawSquare(const Triangle &shape) {
   SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
   SDL_RenderClear(_renderer);
@@ -41,8 +60,14 @@ void Renderer::drawSquare(const Triangle &shape) {
   SDL_RenderDrawLine(_renderer, point2.x, point2.y, point3.x, point3.y);
   // Line 3
   /*SDL_RenderDrawLine(_renderer, point3.x, point3.y, point1.x, point1.y);*/
-  SDL_RenderPresent(this->getRender());
+  SDL_RenderPresent(_renderer);
 }
+
+void Renderer::clear() {
+  SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
+  SDL_RenderClear(_renderer);
+  // SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255);
+};
 
 void Renderer::drawTriangle(const Triangle &shape) {
   /*clear();*/
@@ -76,6 +101,9 @@ void Renderer::drawTriangle(const Triangle &shape) {
   // Line 3
   SDL_RenderDrawLine(_renderer, point3.x, point3.y, point1.x, point1.y);
 }
+
+void Renderer::presentRender() { SDL_RenderPresent(_renderer); }
+
 void Renderer::cleanUp() {
   SDL_DestroyRenderer(_renderer);
   SDL_DestroyWindow(_window);
@@ -87,14 +115,4 @@ void Renderer::printPoints(SDL_FPoint a, SDL_FPoint b, SDL_FPoint c) {
   std::cout << "B->(" << b.x << ',' << b.y << ')' << std::endl;
   std::cout << "C->(" << c.x << ',' << c.y << ')' << std::endl;
   std::cout << "==========\n";
-}
-
-void Renderer::drawSquares(const Mesh &shape) {
-  std::cout << __PRETTY_FUNCTION__ << "::" << __LINE__
-            << ": drawing moved triangles\n";
-  for (auto &tri : shape.tris) {
-    cout << tri.p1 << tri.p2 << tri.p3;
-    drawSquare(tri);
-    SDL_RenderPresent(_renderer);
-  }
 }
