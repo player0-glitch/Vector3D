8afb8ed2f1d554fbad76c9afdd3aae072b29b4f5
player0-glitch <mokhaditlhalefo@gmail.com>
2025-07-29 15:40:42 +0200

    refactor(renderer): no major changes to the header

--- include/Renderer.h
+++ include/Renderer.h
@@ -5,7 +5,7 @@
 #include "SDL2/SDL_render.h"
 #include "SDL2/SDL_video.h"
 #include "v3Util.h"
-
+#include <cxxabi.h>
 extern const int WINDOW_WIDTH, WINDOW_HEIGHT;
 extern const float VIEWPORT_WIDTH, VIEWPORT_HEIGHT;
 
@@ -14,19 +14,22 @@ class Renderer {
 public:
   Renderer(const char *windowTitle, int windowWidth, int windowHeight);
   Renderer(const Renderer &&) = delete; // wtf are you doing
+  Renderer(const Renderer &) = delete;  // no copying
   ~Renderer() = default;
   void drawSquare(const Triangle &shape);
   void drawSquares(const Mesh &shape);
   void render(const Mesh &shape);
-  /*void render(SDL_Texture *texture);*/
   void clear(); // call SDL_Renderer clear
   void display();
   void drawTriangle(const Triangle &shape);
   void cleanUp();
   // i'll implement this in the future
   SDL_Renderer *getRender() const { return _renderer; }
+  void setRender(SDL_Renderer *r) { _renderer = r; }
+  void presentRender();
   void printPoints(SDL_FPoint a, SDL_FPoint b, SDL_FPoint c);
   float getFrameDelay() const { return _frameDelay; }
+  void draw(const Mesh &m);
 
 private:
   float _FOV = 10.0f;
@@ -37,5 +40,17 @@ private:
   SDL_Window *_window;
   SDL_Renderer *_renderer;
   Mesh _mesh; // a collection of triangles that make up a geometric shape
+              //
+  // template <typename T> void printType() {
+  //   int stat{0};
+  //   char *demangled =
+  //       abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &stat);
+  //   if (stat == 0) {
+  //     std::cout << "Type: " << demangled << std::endl;
+  //     free(demangled);
+  //   } else {
+  //     std::cout << "Type: " << typeid(T).name() << std::endl;
+  //   }
+  //  };
 };
 #endif
